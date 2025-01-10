044cd188bceb532ad220aab2c343665dec195c03
anujbarve <anujbarve27@gmail.com>
2025-01-10 13:40:47 +0530

    use site url

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,20 +3,21 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
+  const siteURL = process.env.SITE_URL; // Using SITE_URL from environment variables
+
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
-      const supabase = await createClient();
       const { data, error: userError } = await supabase.auth.getUser();
       if (userError) {
         console.log("Error fetching user data : ", userError.message);
-        return NextResponse.redirect(`${origin}/error`);
+        return NextResponse.redirect(`${siteURL}/error`);
       }
 
       // Create a user instance in users table
@@ -33,24 +34,24 @@ export async function GET(request: Request) {
           username: data.user.user_metadata.username,
         });
         if (insertError) {
-            console.log("Error inserting user data : ", insertError.message);
-            return NextResponse.redirect(`${origin}/error`);
+          console.log("Error inserting user data : ", insertError.message);
+          return NextResponse.redirect(`${siteURL}/error`);
         }
       }
 
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      const forwardedHost = request.headers.get("x-forwarded-host");
       const isLocalEnv = process.env.NODE_ENV === "development";
+
       if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(`${siteURL}${next}`);
       } else if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(`${siteURL}${next}`);
       }
     }
   }
 
-  // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  // Return the user to an error page with instructions
+  return NextResponse.redirect(`${siteURL}/auth/auth-code-error`);
 }
