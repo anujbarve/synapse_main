11a8c1df98e6a1759277f5e659664e45c704927c
anujbarve <anujbarve27@gmail.com>
2025-01-10 12:41:59 +0530

    reverting

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
@@ -13,44 +13,25 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       const supabase = await createClient();
-      const { data, error: userError } = await supabase.auth.getUser();
-      if (userError) {
-        console.log("Error fetching user data : ", userError.message);
-        return NextResponse.redirect(`${process.env.SITE_URL}/error`);
+      const { data,error : userError } = await supabase.auth.getUser();
+      if(userError){
+        console.log("Error fetching user data : ",userError.message);
+        return NextResponse.redirect(`${origin}/error`)
       }
-
-      // Create a user instance in users table
-      const { data: existingUser } = await supabase
-        .from("users")
-        .select("*")
-        .eq("email", data.user.email)
-        .limit(1)
-        .single();
-
-      if (!existingUser) {
-        const { error: insertError } = await supabase.from("users").insert({
-          email: data.user.email,
-          username: data.user.user_metadata.username,
-        });
-        if (insertError) {
-            console.log("Error inserting user data : ", insertError.message);
-            return NextResponse.redirect(`${process.env.SITE_URL}/error`);
-        }
-      }
-
+        
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${process.env.SITE_URL}${next}`);
+        return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
-        return NextResponse.redirect(`${process.env.SITE_URL}${next}`);
+        return NextResponse.redirect(`${origin}${next}`);
       }
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${process.env.SITE_URL}/auth/auth-code-error`);
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
