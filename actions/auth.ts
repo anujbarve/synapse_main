1290765162c156248ff91d3a97f789c8698fcbc3
anujbarve <anujbarve27@gmail.com>
2025-01-10 12:12:08 +0530

    oauth added

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -6,101 +6,145 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 
-export async function signUp(formData:FormData) {
+export async function signUp(formData: FormData) {
+  const supabase = await createClient();
 
-    const supabase = await createClient();
+  const credentials = {
+    username: formData.get("username") as string,
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-    const credentials = {
-        username : formData.get("username") as string,
-        email : formData.get("email") as string,
-        password : formData.get("password") as string,
-    }
+  const { error, data } = await supabase.auth.signUp({
+    email: credentials.email,
+    password: credentials.password,
+    options: {
+      data: {
+        username: credentials.username,
+      },
+    },
+  });
 
-    const { error, data } = await supabase.auth.signUp({
-        email : credentials.email,
-        password : credentials.password,
-        options : {
-            data : {
-                username : credentials.username
-            }
-        }
-    });
-
-    if(error) {
-        return {
-            status : error.message,
-            user : null
-        }
-    } else if (data.user?.identities?.length === 0 ) {
-        return {
-            status : " User with this email already exists, please login",
-            user : null
-        }
-    }
-    
-
-    revalidatePath("/","layout");
+  if (error) {
     return {
-        status : "success",
-        user : data.user
+      status: error.message,
+      user: null,
     };
+  } else if (data.user?.identities?.length === 0) {
+    return {
+      status: " User with this email already exists, please login",
+      user: null,
+    };
+  }
+
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    user: data.user,
+  };
 }
 
-export async function signIn(formData:FormData) {
-    const supabase = await createClient();
+export async function signIn(formData: FormData) {
+  const supabase = await createClient();
 
-    const credentials = {
-        email : formData.get("email") as string,
-        password : formData.get("password") as string,
-    }
+  const credentials = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-    const { error, data } = await supabase.auth.signInWithPassword(credentials);
+  const { error, data } = await supabase.auth.signInWithPassword(credentials);
 
-    if(error) {
-        return {
-            status : error.message,
-            user : null
-        }
-    }
-    
-    // Create a user instance in users table
-    const {data : existingUser} = await supabase
+  if (error) {
+    return {
+      status: error.message,
+      user: null,
+    };
+  }
+
+  // Create a user instance in users table
+  const { data: existingUser } = await supabase
     .from("users")
     .select("*")
-    .eq("email",credentials.email)
+    .eq("email", credentials.email)
     .limit(1)
     .single();
 
-    if(!existingUser){
-        const {error:insertError} = await supabase.from("users")
-        .insert({
-            email : data.user.email,
-            username : data.user.user_metadata.username
-        });
-        if(insertError){
-            return {
-                status : insertError.message,
-                user : null
-            }
-        }
+  if (!existingUser) {
+    const { error: insertError } = await supabase.from("users").insert({
+      email: data.user.email,
+      username: data.user.user_metadata.username,
+    });
+    if (insertError) {
+      return {
+        status: insertError.message,
+        user: null,
+      };
     }
+  }
 
-    revalidatePath("/","layout");
-    return {
-        status : "success",
-        user : data.user
-    };
+  revalidatePath("/", "layout");
+  return {
+    status: "success",
+    user: data.user,
+  };
 }
 
 export async function logOut() {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const { error } = await supabase.auth.signOut();
+  const { error } = await supabase.auth.signOut();
 
-    if(error) {
-        redirect("/error");
-    }
+  if (error) {
+    redirect("/error");
+  }
 
-    revalidatePath("/","layout");
-    redirect("/login");
+  revalidatePath("/", "layout");
+  redirect("/login");
 }
+
+export async function getUserSession() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    return null;
+  }
+  return {
+    status: "success",
+    user: data?.user,
+  };
+}
+
+export async function signInWithGithub() {
+  const origin = (await headers()).get("origin");
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    redirect("/error");
+  } else if (data.url) {
+    return redirect(data.url);
+  }
+}
+
+
+export async function signInWithGoogle() {
+    const origin = (await headers()).get("origin");
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${origin}/auth/callback`,
+      },
+    });
+  
+    if (error) {
+      redirect("/error");
+    } else if (data.url) {
+      return redirect(data.url);
+    }
+  }
--- components/register-form.tsx
+++ components/register-form.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/navigation";
 import { signUp } from "@/actions/auth";
 import AuthButton from "./auth-button";
 import Image from "next/image";
+import OAuthButton from "./oauth-button";
 
 export function RegisterForm({
   className,
@@ -82,19 +83,8 @@ export function RegisterForm({
                 </span>
               </div>
               <div className="grid grid-cols-2 gap-4">
-                <Button variant="outline" className="w-full">
-                  <GithubIcon></GithubIcon>
-                  <span className="sr-only">Login with Github</span>
-                </Button>
-                <Button variant="outline" className="w-full">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span className="sr-only">Login with Google</span>
-                </Button>
+              <OAuthButton type="Github"></OAuthButton>
+              <OAuthButton type="Google"></OAuthButton>
               </div>
               <div className="text-center text-sm">
                 Have an account?{" "}
